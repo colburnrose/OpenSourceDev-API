90e9bd91d3bc6fa0619b30c901a9ba11bead40e4
colburncodes <colburnsanders@gmail.com>
2021-04-25 09:05:03 -0500

    change

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,14 +4,18 @@ const jwt = require("jsonwebtoken");
 
 // REGISTER USER ======================
 const register = async (req, res) => {
-  const params = ({ name, email, password } = req.body);
+  const { name, email, password } = req.body;
+
+  const params = { name, email, password };
 
   // validation
-  if (!name) return res.status(400).send("Name is required");
-  if (!password || password.length < 6)
-    return res
-      .status(400)
-      .send("Password is required and should be minimum 6 characters long");
+  for (const [key, value] of Object.entries(params)) {
+    if (value == null) {
+      return res.status(400).send({
+        error: { message: `Missing '${key}' in the request body` },
+      });
+    }
+  }
 
   let userExist = await User.findOne({ email: email });
   if (userExist) return res.status(400).send("Email is already taken");
@@ -38,7 +42,11 @@ const login = async (req, res) => {
 
   user.comparePassword(req.body.password, (err, match) => {
     if (!match) {
-      return res.status(400).send("Wrong password!", err);
+      return res.status(400).json({
+        error: {
+          message: err,
+        },
+      });
     }
     let token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
